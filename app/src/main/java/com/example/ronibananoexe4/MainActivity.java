614fae496dc57af9688f7f3a1d61b71c1df30380
מיטל <ronni.ban@sisma.org.il>
2024-01-02 15:52:50 +0200

    second commit

--- app/src/main/java/com/example/ronibananoexe4/MainActivity.java
+++ app/src/main/java/com/example/ronibananoexe4/MainActivity.java
@@ -3,12 +3,25 @@ package com.example.ronibananoexe4;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button bt;
+    int count =0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bt=findViewById(R.id.button);
+    }
+
+    public void exe(View view) {
+        count++;
+        bt.setText("this is a click number: "+count);
+        if(count%7==0){
+            bt.setText("BOOM!");
+        }
     }
 }
